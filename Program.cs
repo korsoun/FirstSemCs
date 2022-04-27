2e2cbef7c1c12669538eb3d13e23292a174a558e
alexkors <korsun55555@mail.ru>
2022-04-27 21:40:48 +0700

    Решение первой задачи

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Прием двух чисел и вывод большего
+
+Console.WriteLine("Введите первое число");
+double number1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double number2 = Convert.ToDouble(Console.ReadLine());
+if(number1 > number2) Console.Write("Большее число: " + number1);
+if(number2 > number1) Console.Write("Большее число: " + number2);
+if(number1 == number2) Console.Write("Эти числа равны");
+
