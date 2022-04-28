c7d563c2011b988816019bcfb2ab328f92d56b41
alexkors <korsun55555@mail.ru>
2022-04-28 19:59:47 +0700

    решение третьей задачи

---
+++ EvenNumbers/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 3. Принять число и проверить, четное ли оно
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 2 == 0) Console.Write("Число четное");
+if(number % 2 != 0) Console.Write("Число нечетное");
+
