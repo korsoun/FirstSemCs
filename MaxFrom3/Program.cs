e937eb8c4031f620fdd4484a8e435b64c483a033
alexkors <korsun55555@mail.ru>
2022-04-28 19:48:40 +0700

    решение второй задачи

---
+++ MaxFrom3/Program.cs
@@ -0,0 +1,14 @@
+﻿// взять 3 числа и вывести максимальное
+
+Console.WriteLine("Введите первое число");
+double number1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double number2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+double number3 = Convert.ToDouble(Console.ReadLine());
+double max = number1;
+if(number2 > max) max = number2;
+if(number3 > max) max = number3;
+Console.Write("Наибольшее число: " + max);
+
+
